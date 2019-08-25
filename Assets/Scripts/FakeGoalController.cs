b5182b1c6eafc79a1b13bf115fe983ff228fb937
Kayne Ruse <kayneruse@gmail.com>
2019-08-25 19:04:38 +1000

    Built out a basic level

---
+++ Assets/Scripts/FakeGoalController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FakeGoalController : MonoBehaviour {
+	Rigidbody2D rigidBody;
+
+	void Awake() {
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+		rigidBody.rotation += 1;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		SceneManager.LoadScene("FakeWin");
+	}
+}
---
+++ Assets/Scripts/FakeWinScene/LevelController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour {
+	void Update() {
+		GamePadState state = GamePad.GetState();
+
+		bool input = false;
+		const float deadZone = 0.25f;
+
+		//any input
+		input |= state.Pressed(CButton.A) || state.Pressed(CButton.B) || state.Pressed(CButton.X) || state.Pressed(CButton.Y);
+		input |= state.Pressed(CButton.Back) || state.Pressed(CButton.Start);
+		input |= Mathf.Abs(state.GetAxis(CAxis.LX)) > deadZone || Mathf.Abs(state.GetAxis(CAxis.LY)) > deadZone || Mathf.Abs(state.GetAxis(CAxis.RX)) > deadZone || Mathf.Abs(state.GetAxis(CAxis.RY)) > deadZone;
+
+		if (input) {
+			SceneManager.LoadScene("GamePlay");
+		}
+	}
+}
