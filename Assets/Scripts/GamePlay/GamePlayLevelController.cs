9ddcbb06decd4daed53cad212fea0d2f30f1189a
Kayne Ruse <kayneruse@gmail.com>
2019-08-30 06:47:08 +1000

    Kind of ready for release

---
+++ Assets/Scripts/GamePlay/GamePlayLevelController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayLevelController : MonoBehaviour {
+	void Update() {
+		if (Input.GetKey("escape")) {
+			Application.Quit(0);
+		}
+	}
+}
